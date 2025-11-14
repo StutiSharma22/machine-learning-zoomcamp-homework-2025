189c204e00aaa70905277e136cd215cbc045eef2
Stuti Sharma <52918756+StutiSharma22@users.noreply.github.com>
2025-11-14 18:55:25 +0000

    update

--- 07-mid-term-project/app.py
+++ 07-mid-term-project/app.py
@@ -62,4 +62,4 @@ def predict():
     return jsonify({'prediction': round(pred, 2)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
